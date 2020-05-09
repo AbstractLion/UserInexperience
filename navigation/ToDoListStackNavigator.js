829e72bf35d4fd9eb579e3b54918dfc94973eda2
Leon Si <leonzalion@gmail.com>
2020-05-09 10:46:48 -0400

    changes

---
+++ navigation/ToDoListStackNavigator.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { createStackNavigator } from "@react-navigation/stack";
+import TodoListScreen from "../screens/TodoListScreen";
+import TodoListItemDetailsScreen from "../screens/TodoListItemDetailsScreen";
+
+const Stack = createStackNavigator();
+
+export default function TodoListStackNavigator() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen
+        name={"TodoList"}
+        component={TodoListScreen}
+      />
+      <Stack.Screen
+        name={"TodoListDetails"}
+        component={TodoListItemDetailsScreen}
+      />
+    </Stack.Navigator>
+  );
+}
