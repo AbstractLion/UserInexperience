bf48645a703b61545e2fe55d5c7c9218d6b12ef3
AbstractUltra <dragonhe36@gmail.com>
2020-05-09 12:19:44 -0400

    A

--- screens/IntroScreen.js
+++ screens/IntroScreen.js
@@ -1,43 +1,50 @@
-import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
 import AppIntroSlider from "react-native-app-intro-slider";
 
 const slides = [
   {
     key: 1,
-    title: 'User Inexperience',
-    text: 'This app is intended to have awful UX',
-    bg: '#59b2ab',
+    title: "User Inexperience",
+    text: "This app is intended to have awful UX",
+    bg: "#59b2ab",
   },
   {
     key: 2,
-    title: 'UI',
-    text: 'But we have good UI',
-    bg: '#febe29',
+    title: "UI",
+    text: "But we have good UI",
+    bg: "#febe29",
   },
 ];
 
-function _renderSlide({item, index, dimensions}) {
+function _renderSlide({ item, index, dimensions }) {
   return (
-    <View style={[styles.slide, {backgroundColor: item.bg}]}>
+    <View style={[styles.slide, { backgroundColor: item.bg }]}>
       <Text style={styles.title}>{item.title}</Text>
       <Text style={styles.text}>{item.text}</Text>
     </View>
-  )
+  );
 }
 
-export default function IntroScreen({onDone}) {
+export default function IntroScreen({ onDone }) {
   return (
-    <AppIntroSlider data={slides} showSkipButton={true} showPrevButton={true} renderItem={_renderSlide} onDone={onDone}/>
-  )
+    <AppIntroSlider
+      data={slides}
+      showSkipButton={true}
+      showPrevButton={true}
+      renderItem={_renderSlide}
+      keyExtractor={(item) => item.key.toString()}
+      onDone={onDone}
+    />
+  );
 }
 
 const styles = StyleSheet.create({
   slide: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: 'blue',
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "blue",
   },
   image: {
     width: 320,
@@ -45,12 +52,12 @@ const styles = StyleSheet.create({
     marginVertical: 32,
   },
   text: {
-    color: 'rgba(255, 255, 255, 0.8)',
-    textAlign: 'center',
+    color: "rgba(255, 255, 255, 0.8)",
+    textAlign: "center",
   },
   title: {
     fontSize: 22,
-    color: 'white',
-    textAlign: 'center',
+    color: "white",
+    textAlign: "center",
   },
 });
