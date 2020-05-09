a7f1afa8b577802a9909a269edbbf7353e52feb1
AbstractUltra <dragonhe36@gmail.com>
2020-05-09 17:52:03 -0400

    a

--- screens/ads/GeoPro.js
+++ screens/ads/GeoPro.js
@@ -1,8 +1,11 @@
 import React, { useEffect, useState } from "react";
 import { View, Text } from "react-native";
+import { Dropdown } from "react-native-material-dropdown";
 
 export default function GeoProAd() {
   const [countries, setCountries] = useState({});
+  const [countryList, setCountryList] = useState([]);
+  const [currentCountry, setCurrentCountry] = useState("Canada");
   useEffect(() => {
     fetch("https://restcountries-v1.p.rapidapi.com/all", {
       method: "GET",
@@ -14,6 +17,7 @@ export default function GeoProAd() {
       .then((response) => response.json())
       .then((data) => {
         let obj = {};
+        let arr = [];
         for (let i = 0; i < data.length; i++) {
           const item = data[i];
           obj[item.name] = {
@@ -22,19 +26,24 @@ export default function GeoProAd() {
             region: item.region,
             population: item.population,
           };
+          arr.push({value: item.name});
         }
+        setCountryList(arr);
         setCountries(obj);
       });
   }, []);
   return (
-    <View>
+    <View style={{margin:40}}>
       <Text>
         Failing Geography? No problem! Our phone app will let you cheat on your
         geography tests at ease. Test a demo below!
       </Text>
-      <Text>
-        {countries ? countries["Canada"].capital : null}
-      </Text>
+      <Dropdown label={"Countries"} data={countryList} onChangeText={(string) => setCurrentCountry(string)}/>
+      <View style={{flex: 1, alignItems: 'center', margin: 30}}>
+        <Text>Capital: {countries[currentCountry].capital}</Text>
+        <Text>Region: {countries[currentCountry].region}</Text>
+        <Text>Population: {countries[currentCountry].population}</Text>
+      </View>
     </View>
   );
 }
